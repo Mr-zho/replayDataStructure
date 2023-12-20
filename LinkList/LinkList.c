796e036bb3b2e5cc2003008bd7512c36d6313dc7
wenliang <wenliang@163.com>
2023-12-20 14:49:17 +0800

    [修改]完善删除API接口

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -128,19 +128,54 @@ int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTYPE val)
 /* 链表头删 */
 int LinkListHeadDel(LinkList * pList)
 {
-    
+    return LinkListDelAppointPos(pList, 1);
 }
 
 /* 链表尾删 */
 int LinkListTailDel(LinkList * pList)
 {
-    
+    return LinkListDelAppointPos(pList, pList->len);
 }
 
 /* 链表指定位置删 */
 int LinkListDelAppointPos(LinkList * pList, int pos)
 {
+    int ret = 0;
+    if (pList == NULL)
+    {
+        return NULL_PTR;
+    }
     
+    if (pos <= 0 || pos > pList->len)
+    {
+        return INVALID_ACCESS;
+    }
+
+#if 1
+    LinkNode * travelNode = pList->head;
+#else
+    LinkNode * travelNOde = pList->head->next;
+#endif 
+
+    while (--pos)
+    {
+        /* 向后移动位置 */
+        travelNode = travelNode->next;
+    }   
+    // 跳出循环找到的是哪一个结点？
+    LinkNode * needDelNode = travelNode->next;
+    travelNode->next = needDelNode->next; 
+
+    /* 释放内存 */
+    if (needDelNode != NULL)
+    {
+        free(needDelNode);
+        needDelNode = NULL;
+    }
+
+    /* 链表长度减一 */
+    (pList->len)--;
+    return ret;
 }
 
 /* 链表删除指定的数据 */
