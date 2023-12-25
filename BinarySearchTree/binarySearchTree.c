88691abb8f90f5d7dccc168da0882cd4331b938e
wenliang <wenliang@163.com>
2023-12-25 19:31:49 +0800

    [修改] 使用队列的思想完成二叉搜索树的层序遍历

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -1,6 +1,7 @@
 #include "binarySearchTree.h"
 #include <stdlib.h>
 #include <string.h>
+#include "doubleLinkListQueue.h"
 
 /* 状态码 */
 enum STATUS_CODE
@@ -208,7 +209,6 @@ int binarySearchTreePreOrderTravel(BinarySearchTree *pBstree)
 int binarySearchTreeInOrderTravel(BinarySearchTree *pBstree)
 {
     int ret = 0;
-
     return ret;
 }
 
@@ -224,7 +224,35 @@ int binarySearchTreePostOrderTravel(BinarySearchTree *pBstree)
 int binarySearchTreeLevelOrderTravel(BinarySearchTree *pBstree)
 {
     int ret = 0;
+    DoubleLinkListQueue * pQueue = NULL;
+    doubleLinkListQueueInit(&pQueue);
 
+    /* 1. 根结点入队 */
+    doubleLinkListQueuePush(pQueue, pBstree->root);
+
+    /* 2. 判断队列是否为空 */
+    BSTreeNode *nodeVal = NULL;
+    while (!doubleLinkListQueueIsEmpty(pQueue))
+    {
+        doubleLinkListQueueTop(pQueue, (void **)&nodeVal);
+        printf ("data:%d\n", nodeVal->data);
+        doubleLinkListQueuePop(pQueue);
+
+        /* 将左子树入队. */
+        if (nodeVal->left != NULL)
+        {
+            doubleLinkListQueuePush(pQueue, nodeVal->left);
+        }
+
+        /* 将右子树入队. */
+        if (nodeVal->right != NULL)
+        {
+            doubleLinkListQueuePush(pQueue, nodeVal->right);
+        }
+    }
+
+    /* 释放队列 */
+    doubleLinkListQueueDestroy(pQueue);
     return ret;
 }
 
@@ -258,5 +286,5 @@ static BSTreeNode * baseAppointValGetBSTreeNode(BinarySearchTree *pBstree, ELEME
 /* 二叉搜索树是否包含指定的元素 */
 int binarySearchTreeIsContainAppointVal(BinarySearchTree *pBstree, ELEMENTTYPE val)
 {
-    baseAppointValGetBSTreeNode(pBstree, val);
+    return baseAppointValGetBSTreeNode(pBstree, val) == NULL ? 0 : 1;
 }
