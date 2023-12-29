41654266da47a876f92a13dea0b28891733dd1e8
wenliang <wenliang@163.com>
2023-12-29 18:08:29 +0800

    [修改] 完善平衡二叉搜索树的测试用例

--- BalanceBinarySearchTree/main.c
+++ BalanceBinarySearchTree/main.c
@@ -2,7 +2,7 @@
 #include "balanceBinarySearchTree.h"
 
 
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 3
 
 /* 测试二叉搜索树 */
 int compareBasicDataFunc(void * arg1, void *arg2)
@@ -28,9 +28,9 @@ int main()
     BalanceBinarySearchTree * AVL;
     balanceBinarySearchTreeInit(&AVL, compareBasicDataFunc, printBasicData);
 
-    int buffer[BUFFER_SIZE] = {56, 28, 75, 73, 77, 13, 7, 26, 100, 12};
+    int buffer[] = {13, 14, 15, 12, 11, 17, 16, 8, 9, 1};
 
-    for (int idx = 0; idx < BUFFER_SIZE; idx++)
+    for (int idx = 0; idx < sizeof(buffer) /sizeof(buffer[0]); idx++)
     {   
         balanceBinarySearchTreeInsert(AVL, (void *)&buffer[idx]);
     }
@@ -44,7 +44,31 @@ int main()
     balanceBinarySearchTreeGetHeight(AVL, &height);
     printf("height:%d\n", height);
 
+    /* 层序遍历 */
+    balanceBinarySearchTreeLevelOrderTravel(AVL);
+    printf("\n");
 
+
+    int delVal1 = 11;
+    balanceBinarySearchTreeDelete(AVL, &delVal1);
+
+    int delVal2 = 12;
+    balanceBinarySearchTreeDelete(AVL, &delVal2);
+
+    int delVal3 = 13;
+    balanceBinarySearchTreeDelete(AVL, &delVal3);
+
+
+    /* 获取二叉搜索树的高度 */
+    height = 0;
+    balanceBinarySearchTreeGetHeight(AVL, &height);
+    printf("height:%d\n", height);
+
+    /* 层序遍历 */
+    balanceBinarySearchTreeLevelOrderTravel(AVL);
+    printf("\n");
+
+#if 0
     /* 中序遍历 */
     balanceBinarySearchTreeInOrderTravel(AVL);
     printf("\n");
@@ -112,7 +136,7 @@ int main()
     /* 层序遍历 */
     balanceBinarySearchTreeLevelOrderTravel(AVL);
     printf("\n");
-
+#endif
 
 
     return 0;
