8f9215e90e98992e097f75b98edf7e09dbf3362b
wenliang <wenliang@163.com>
2024-01-16 12:59:20 +0800

    [新增] 新增哈希表的结构和接口函数

---
+++ HashTable/hashtable.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "hashtable.h"
+
+
+/* 哈希表的初始化 */
+int hashTableInit(HashTable* pHashtable)
+{
+    int ret = 0;
+
+    return ret;
+}
+
+/* 哈希表 插入<key, value> */
+int hashTableInsert(HashTable *pHashtable, int key, int value)
+{
+    int ret = 0;
+
+    return ret;
+}
+
+/* 哈希表 删除指定key. */
+int hashTableDelAppointKey(HashTable *pHashtable, int key)
+{
+    int ret = 0;
+
+    return ret;
+}
+
+/* 哈希表 根据key获取value. */
+int hashTableGetAppointKeyValue(HashTable *pHashtable, int key)
+{
+    int ret = 0;
+
+    return ret;
+}
